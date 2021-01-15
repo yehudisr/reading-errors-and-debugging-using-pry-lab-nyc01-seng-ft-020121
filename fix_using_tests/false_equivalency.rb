ddcff7aca6963105eb51658c2caf73c13aac2357
yehudis raitman <yehudisr@gmail.com>
2021-01-15 18:27:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -10,11 +11,12 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
+
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  if num == 1
+    "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+    "HAM HAM HAM IN MY TUMMY"
   end
 end
 
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,11 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
+ binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    "s" * 10 + string
   else
     string
   end
